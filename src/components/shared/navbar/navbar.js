16d7a086f3e9153e02769d01c7434c4166f588fb
anhi67srm <ranzo9430@gmail.com>
2020-01-26 19:43:10 +0530

    Added style for contact/about page

--- src/components/shared/navbar/navbar.js
+++ src/components/shared/navbar/navbar.js
@@ -35,9 +35,28 @@ const Navbar = () => {
       </ul>
 
       <ul className="navbar__social-container">
-        <li className="navbar__social-container__item">GitHub</li>
-        <li className="navbar__social-container__item">Instagram</li>
-        <li className="navbar__social-container__item">Email</li>
+        <li className="navbar__social-container__item">
+          <a href="https://github.com/abhi67srm" target="_blank">
+            Github
+          </a>
+        </li>
+        <li className="navbar__social-container__item">
+          <li className="navbar__social-container__item">
+            <a
+              href="https://www.instagram.com/__full_stack_developer__/"
+              target="_blank"
+            >
+              Instagram
+            </a>
+          </li>
+        </li>
+        <li className="navbar__social-container__item">
+          <li className="navbar__social-container__item">
+            <a href="mailto:ranzo9430@gmail.com" target="_blank">
+              Email
+            </a>
+          </li>
+        </li>
       </ul>
     </nav>
   )
