c70f639f0030201467a7144ca2036e5f8c82c6e5
anhi67srm <ranzo9430@gmail.com>
2020-02-09 19:16:42 +0530

    Landing page design added

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,11 +6,25 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faHamburger, faInstagram } from "@fortawesome/free-solid-svg-icons"
 import Head from "../components/shared/head"
 import "bootstrap/dist/css/bootstrap.min.css"
+import { Card, Button } from "react-bootstrap"
 
 const HomePage = () => {
   return (
-    <Layout>
+    <Layout className="head-container">
       <Head title="Home" />
+      <div className="head-container__main-text">
+        <Card style={{ width: "18rem" }}>
+          <Card.Img
+            variant="top"
+            src="https://images.unsplash.com/photo-1549692520-acc6669e2f0c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=633&q=80"
+          />
+          <Card.Body>
+            <Card.Title>Hey, I am Abhishek</Card.Title>
+            <Card.Text>I love to design & build website for clients</Card.Text>
+            <Button variant="warning">Contact</Button>
+          </Card.Body>
+        </Card>
+      </div>
       {/* <div className="home-container">
         <p className="home-container__first-main-text">Hello</p>
         <p className="home-container__second-main-text">
